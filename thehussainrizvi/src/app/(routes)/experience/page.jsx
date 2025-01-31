d3c83f05414d1105af45e09a634cd0921726eb1d
Mohd Musaiyab <mohdmusaiyab123@gmail.com>
2025-01-31 15:42:01 +0530

    Basic CleanUp + Structure

---
+++ thehussainrizvi/src/app/(routes)/experience/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>For Shwoing Experience</div>
+  )
+}
+
+export default page
---
+++ thehussainrizvi/src/app/(routes)/work/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>For showing latest blogs episodes and eveything</div>
+  )
+}
+
+export default page
